5f0b5c4e1968cae4b09eee5c5b3961d348675a26
psychoAB <dome.2499@hotmail.com>
2017-01-27 23:08:28 +0700

    can open gamepad

--- main.c
+++ main.c
@@ -7,12 +7,27 @@
 
 #include "hidapi.h"
 
+#define MAX_STR 255
+
+#define LOGITECT_VID 0x046d
+#define GAMEPAD_PID 0xc219
+
 int main(int argc, char *argv[])
 {
+    hid_device *handle;
+    wchar_t wstr[MAX_STR];
     int response;
 
     response = hid_init();
+    handle = hid_open(LOGITECT_VID, GAMEPAD_PID, NULL);
 
+    response = hid_get_manufacturer_string(handle, wstr, MAX_STR);
+    printf("%ls\n", wstr);
+
+    response = hid_get_product_string(handle, wstr, MAX_STR);
+    printf("%ls\n", wstr);
+
+    hid_close(handle);
     response = hid_exit();
 
     return 0;
