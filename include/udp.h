a080a0beeaddd562fcf7c2889c3bf9fad7786f44
psychoAB <natchanan.th@outlook.com>
2020-07-06 18:22:13 +0700

    added the license and copyright notice to the source files

--- include/udp.h
+++ include/udp.h
@@ -1,7 +1,19 @@
 /*
- * Author: Natchanan Thongtem
- * Created on: 01/28/2017
- */
+    Copyright (C) 2017  Natchanan Thongtem
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 struct sockaddr_in init_socket_address(const char *ip_address, const int port);
 void udp_send(int client_socket, char byte, struct sockaddr_in server);
